e75a236523643dd4db7a86341e44420c32bc9d78
BinnyDevel <89769070+BinnyDevel@users.noreply.github.com>
2022-05-26 20:38:29 +1000

    token tbh

--- bot.py
+++ bot.py
@@ -15,4 +15,4 @@ async def on_ready():
 for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
     bot.load_extension(f'cogs.{filename[:-3]}')
-bot.run("yourmother")
+bot.run("yourmother") #token goes here if you wanna host or something tbh i cant be bothered with a JSON
