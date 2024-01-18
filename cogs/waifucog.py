b773b1251b324ab6a67b58a76f483990b5dc3289
BinnyDevel <89769070+BinnyDevel@users.noreply.github.com>
2024-01-19 01:12:08 +1000

    Update waifucog.py

--- cogs/waifucog.py
+++ cogs/waifucog.py
@@ -1,17 +1,11 @@
-import discord
-from discord.commands import Option, slash_command
 from discord.ext import commands
+from discord_slash import cog_ext
 import datetime
 import asyncio
 import pytz
-import random 
-import asyncio
 import requests
-import json
 
-
-
-class dailybinnywaifu(commands.Cog):
+class DailyBinnyWaifu(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.bgtask = self.bot.loop.create_task(self.binny_waifu())
@@ -19,13 +13,17 @@ class dailybinnywaifu(commands.Cog):
     async def binny_waifu(self):
         await self.bot.wait_until_ready()
         binnywaifuchannel = self.bot.get_channel(911174855821496330)
+
         while not self.bot.is_closed():
-            if datetime.datetime.now(pytz.timezone('Australia/Brisbane')).strftime("%H:%M") == "12:00":
+            now_brisbane = datetime.datetime.now(pytz.timezone('Australia/Brisbane'))
+
+            if now_brisbane.hour == 12 and now_brisbane.minute == 0:
                 waifuimage = requests.get("https://api.waifu.pics/sfw/waifu")
                 waifulink = waifuimage.json()
                 await binnywaifuchannel.send(waifulink["url"])
                 await binnywaifuchannel.send("<@911172278090367007>")
+
             await asyncio.sleep(60)
 
 def setup(bot):
-    bot.add_cog(dailybinnywaifu(bot)) 
+    bot.add_cog(DailyBinnyWaifu(bot))
